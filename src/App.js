4fc98a6d076c9d677b5e049d19a2a0c8ff7ec105
Filipov <andrijfilipov73@gmail.com>
2023-05-08 15:38:00 +0300

    clear code and start build app

--- src/App.js
+++ src/App.js
@@ -1,38 +1,5 @@
-import React, { useState, useEffect } from "react";
-import renderTasks from "./indexedDB.js";
-
 function App() {
-  const [newTaskDescription, setNewTaskDescription] = useState("");
-
-  useEffect(() => {
-    renderTasks([]);
-  }, []);
-
-  function handleNewTaskDescriptionChange(event) {
-    setNewTaskDescription(event.target.value);
-  }
-
-  function handleNewTaskSubmit(event) {
-    event.preventDefault();
-    const task = { description: newTaskDescription };
-    renderTasks([task]);
-    setNewTaskDescription("");
-  }
-
-  return (
-    <div>
-      <form onSubmit={handleNewTaskSubmit}>
-        <input
-          type="text"
-          placeholder="Enter task description"
-          value={newTaskDescription}
-          onChange={handleNewTaskDescriptionChange}
-        />
-        <button type="submit">Add Task</button>
-      </form>
-      <ul></ul>
-    </div>
-  );
+  return <div>start</div>;
 }
 
 export default App;
