2d9a5831f9b62b558780a7130fad31bb5e0e7551
Filipov <andrijfilipov73@gmail.com>
2023-05-08 16:14:21 +0300

    add MUI and SASS and build header with buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import Header from "./container/Header";
+import ListItem from "./container/ListItem";
+import Sidebar from "./container/Sidebar";
+
+import { StyledEngineProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
 function App() {
-  return <div>start</div>;
+  return (
+    <div>
+      <StyledEngineProvider injectFirst>
+        <CssBaseline />
+        <Header />
+        <ListItem />
+        <Sidebar />
+      </StyledEngineProvider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/container/Header.jsx
@@ -0,0 +1,35 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import AddIcon from "@mui/icons-material/Add";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import DeleteIcon from "@mui/icons-material/Delete";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar className="toolbar">
+          <div className="active-buttons">
+            <IconButton size="large" color="inherit">
+              <AddIcon />
+            </IconButton>
+            <IconButton size="large" color="inherit">
+              <DeleteIcon />
+            </IconButton>
+            <IconButton size="large" color="inherit">
+              <EditNoteIcon />
+            </IconButton>
+          </div>
+          <div className="search-container">
+            <input type="text" placeholder="Search..." />
+            <button type="submit">Find</button>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+export default Header;
